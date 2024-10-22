5250c38d9d384bb2834cc4f8fa486739d82385c7
Gerardo Lankenau <glankenau@hey.com>
2024-10-22 23:06:16 +0200

    Initial Commit

---
+++ virtusappTests/virtusappTests.swift
@@ -0,0 +1,17 @@
+//
+//  virtusappTests.swift
+//  virtusappTests
+//
+//  Created by Gerardo Moreno Lankenau on 22/10/24.
+//
+
+import Testing
+@testable import virtusapp
+
+struct virtusappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
